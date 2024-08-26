c812ddf6f07cd29e35f21127220328f0cdcd318c
uni <147740687+shunmakk@users.noreply.github.com>
2024-08-27 01:18:38 +0900

    modelとviewsの設定

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    createdDate = models.DateTimeField(auto_now_add=True)
+
+    #データベースのパネルで確認するため
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['completed']
--- todoapp/urls.py
+++ todoapp/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 # 〇〇のURLが来た時、〇〇のviewに関数として渡すパターン分け
 urlpatterns = [
-    path("/", views.taskList)
+    path("", views.taskList) #ルートディレクトリの場合スラッシュをつけない
 ]
