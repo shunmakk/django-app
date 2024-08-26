692d3c25df86b0d9aea930bf67852368b7398efd
uni <147740687+shunmakk@users.noreply.github.com>
2024-08-27 01:52:09 +0900

    関数ベースviewをクラスベースviewに変更

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls  import path
-from . import views
+from .views import TaskList
 
 # 〇〇のURLが来た時、〇〇のviewに関数として渡すパターン分け
 urlpatterns = [
-    path("", views.taskList) #ルートディレクトリの場合スラッシュをつけない
+    path("", TaskList.as_view()) #ルートディレクトリの場合スラッシュをつけない
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+from todoapp.models import Task
 
 # Create your views here.
-def taskList(request):
-    return HttpResponse("<h1>Django</h1>")
+# def taskList(request):
+#     return HttpResponse("<h1>Django</h1>")
+
+class TaskList(ListView):
+    model = Task
