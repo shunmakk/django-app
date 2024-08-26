c27502269109f786a67cdcf9a3cb70e2f7ef8d96
uni <147740687+shunmakk@users.noreply.github.com>
2024-08-27 03:22:05 +0900

    modelsのデータをhtmlで出力

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls  import path
-from .views import TaskList
+from .views import TaskList, TaskDetail
 
 # 〇〇のURLが来た時、〇〇のviewに関数として渡すパターン分け
 urlpatterns = [
-    path("", TaskList.as_view()) #ルートディレクトリの場合スラッシュをつけない
+    path("", TaskList.as_view()), #ルートディレクトリの場合スラッシュをつけない
+    path("task/<int:pk>/", TaskDetail.as_view(), name="task")
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from todoapp.models import Task
 
 # Create your views here.
@@ -9,3 +10,9 @@ from todoapp.models import Task
 
 class TaskList(ListView):
     model = Task
+    context_object_name = "tasks"
+
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = "task"
