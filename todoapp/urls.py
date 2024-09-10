1437f1406d20e95c2142deaaba7b4156d94a7f41
uni <147740687+shunmakk@users.noreply.github.com>
2024-09-11 03:14:16 +0900

    createViewの作成と、formの作成

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls  import path
-from .views import TaskList, TaskDetail
+from .views import TaskList, TaskDetail,TaskCreate
 
 # 〇〇のURLが来た時、〇〇のviewに関数として渡すパターン分け
 urlpatterns = [
-    path("", TaskList.as_view()), #ルートディレクトリの場合スラッシュをつけない
-    path("task/<int:pk>/", TaskDetail.as_view(), name="task")
+    path("", TaskList.as_view(),name="tasks"), #ルートディレクトリの場合スラッシュをつけない
+    path("task/<int:pk>/", TaskDetail.as_view(), name="task"),
+    path("create-task", TaskCreate.as_view(), name="create-task")
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from todoapp.models import Task
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 # Create your views here.
 # def taskList(request):
@@ -16,3 +18,10 @@ class TaskList(ListView):
 class TaskDetail(DetailView):
     model = Task
     context_object_name = "task"
+
+
+class TaskCreate(CreateView):
+    model = Task
+    # 全てのモデル
+    fields =  "__all__"
+    success_url = reverse_lazy("tasks")
